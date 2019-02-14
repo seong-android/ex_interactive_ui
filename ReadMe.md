5471953f5273195727854c35f2994271b9652552
seong-android <seong.c.kang@gmail.com>
2019-02-14 13:01:33 -0600

    initial completion

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# eps02_interactive_ui.android
+> This is a counter app that counts up when the button is pressed. Also, button triggers a toast!
+> This utilizes the basics of user interaction.
+<img src="interactive_ui.jpg" width="225" height="400" />
---
+++ app/src/main/java/com/seongandroid/interactive_ui/MainActivity.java
@@ -0,0 +1,34 @@
+package com.seongandroid.interactive_ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.textview_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, Integer.toString(mCount) , Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount ++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
